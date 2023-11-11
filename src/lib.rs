bf4aa9dd6ec4818ff059c969f75feed631194793
Fanda Vacek <fvacek@elektroline.cz>
2023-11-11 06:41:04 +0100

    Line numbers logging implemented

--- src/lib.rs
+++ src/lib.rs
@@ -410,11 +410,12 @@ impl Log for SimpleLogger {
                 }
             };
 
-            let target = if !record.target().is_empty() {
-                record.target()
+            let target = if record.target().is_empty() || record.target() == record.module_path().unwrap_or_default() {
+                "".to_string()
             } else {
-                record.module_path().unwrap_or_default()
+                format!("({})", record.target())
             };
+            let module = format!("{}:{}", record.module_path().unwrap_or_default(), record.line().unwrap_or_default());
 
             let thread = {
                 #[cfg(feature = "threads")]
@@ -478,7 +479,7 @@ impl Log for SimpleLogger {
                 ""
             };
 
-            let message = format!("{}{} [{}{}] {}", timestamp, level_string, target, thread, record.args());
+            let message = format!("{}{} [{}{}]{} {}", timestamp, level_string, module, thread, target, record.args());
 
             #[cfg(not(feature = "stderr"))]
             println!("{}", message);
